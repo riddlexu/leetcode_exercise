0c0483fd0b163f9303341a94a34469f724cd6705
xyt <xuyangtong@gmail.com>
2014-05-17 15:23:16 +0800

    finish 4 exer

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-default: single_number
+default: max_depth_tree
+WORK=max_depth_tree 
 CC=g++
 CFLAGS=-O3 -Wall -std=c++11
-PROGRAM=reverse_word_in_string evaluate_rpn single_number
+PROGRAM=reverse_word_in_string evaluate_rpn single_number max_depth_tree
 
 all: $(PROGRAM)
 reverse_word_in_string: reverse_word_in_string.cpp
@@ -13,7 +14,10 @@ evaluate_rpn: evaluate_rpn.cpp
 single_number: single_number.cpp
 	$(CC) $(CFLAGS) $< -o $@
 
+max_depth_tree: max_depth_tree.cpp
+	$(CC) $(CFLAGS) $< -o $@
+
 
 .PHONY: clean
 clean:
-	rm $(PROGRAM)	
+	rm $(WORK)	
---
+++ max_depth_tree.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+class Solution {
+ public:
+  int maxDepth(TreeNode *root) {
+    if (root == NULL){
+      return 0;
+    }
+    int l = maxDepth(root->left);
+    int r = maxDepth(root->right);
+
+    if (l > r){
+      return l + 1;
+    }
+    else {
+      return r + 1;
+    }
+    return 0;
+  }
+};
+
+int main()
+{
+  Solution solution;
+  TreeNode tree(2);
+  cout << solution.maxDepth(&tree) << endl;
+  return 0;
+}
