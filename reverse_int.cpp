0cade68bada0be531291510babd609d7aba61907
xyt <xuyangtong@gmail.com>
2014-05-17 18:11:42 +0800

    add reverse int

---
+++ reverse_int.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+ public:
+  int reverse(int x) {
+    if (x == 0){
+      return 0;
+    }
+    int num = 0;
+    int y = x;
+    while(y != 0){
+      
+    }
+  }
+};
+int main()
+{
+  return 0;
+}
