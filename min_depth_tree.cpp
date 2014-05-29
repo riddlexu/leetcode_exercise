3fc629da04d41c823d085e1bba156eef388d0ecb
xyt <xuyangtong@gmail.com>
2014-05-29 17:50:14 +0800

    start min depth

--- min_depth_tree.cpp
+++ min_depth_tree.cpp
@@ -1,15 +1,26 @@
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include<iostream>
+using namespace std;
+
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
-    int minDepth(TreeNode *root) {
+  int minDepth(TreeNode *root) {
         
-    }
+  }
 };
+
+
+int main()
+{
+  Solution solution;
+  TreeNode tree(2);
+  cout << solution.minDepth(&tree) << endl;
+  return 0;
+}
