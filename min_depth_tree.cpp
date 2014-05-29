9e6b2eeef51cb9aa069c39984fb2c5367c30b8c0
xyt <xuyangtong@gmail.com>
2014-05-29 19:29:38 +0800

    finish min depth tree

--- min_depth_tree.cpp
+++ min_depth_tree.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 
-
 struct TreeNode {
   int val;
   TreeNode *left;
@@ -10,9 +9,28 @@ struct TreeNode {
 };
 
 class Solution {
-public:
+ public:
   int minDepth(TreeNode *root) {
-        
+    if (root == NULL){
+      return 0;
+    }
+    if ((root->left == NULL) && (root->right == NULL)){
+      return 1;
+    }
+    if (root->left == NULL){
+      return 1 + minDepth(root->right);
+    }
+    if (root->right == NULL){
+      return 1 + minDepth(root->left);
+    }
+    int l = minDepth(root->left);
+    int r = minDepth(root->right);
+    if (l < r){
+      return l + 1;
+    }
+    else {
+      return r + 1;
+    }
   }
 };
 
