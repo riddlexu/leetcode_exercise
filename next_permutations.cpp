38a72e9b5e8f536a437b784a74fa8dd5fc968a26
xyt <xuyangtong@gmail.com>
2014-05-30 19:38:50 +0800

    start next permutations

--- next_permutations.cpp
+++ next_permutations.cpp
@@ -5,7 +5,47 @@ using namespace std;
 class Solution {
  public:
   void nextPermutation(vector<int> &num) {
-        
+    bool flag = false;
+    int loc;
+    if (num.size() == 0){
+      return;
+    }
+    for (unsigned int i = num.size() - 1; i > 0; i--){
+      if (num[i] > num[i-1]){
+        loc = i;
+        flag = true;
+        break;
+      }
+    }
+    int temp;
+    unsigned int start;
+    unsigned int end;
+    if (flag){
+      temp = num[loc];
+      num[loc] = num[loc-1];
+      num[loc-1] = temp;
+      start = loc + 1;
+      end = num.size() - 1;
+      while(start < end){
+        temp = num[start];
+        num[start] = num[end];
+        num[end] = temp;
+        start++;
+        end--;
+      }
+
+    }
+    else{
+      start = 0;
+      end = num.size() - 1;
+      while(start < end){
+        temp = num[start];
+        num[start] = num[end];
+        num[end] = temp;
+        start++;
+        end--;
+      }
+    }
   }
 };
 
@@ -19,9 +59,9 @@ int main()
     num.push_back(temp);
   }
   Solution solution;
-  solution.permute(num);
+  solution.nextPermutation(num);
   for (unsigned int i = 0; i < num.size(); i++){
-    cout << num[i] << endl;
+    cout << num[i];
   }
 }
 
