d4bb3e89ae58974c58f64f8e5709482ad89bbc28
xyt <xuyangtong@gmail.com>
2014-05-30 15:15:27 +0800

    finish permutations

--- permutations.cpp
+++ permutations.cpp
@@ -3,10 +3,31 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<vector<int> > permute(vector<int> &num) {
-        
+ public:
+  vector<vector<int> > permute(vector<int> &num) {
+    vector<vector<int> > ans;
+    vector<int> start;
+    permute_rec(ans,start,num);
+    return ans;
+  }
+  void permute_rec(vector<vector<int> > &ans, vector<int> start, vector<int> left){
+    if (left.size() == 0){
+      ans.push_back(start);
+      return;
     }
+    if (left.size() == 1){
+      start.push_back(left[0]);
+      ans.push_back(start);
+      return;
+    }
+    for (unsigned int i = 0; i < left.size(); i++){
+      vector<int> new_start = start;
+      vector<int> new_left = left;
+      new_start.push_back(left[i]);
+      new_left.erase(new_left.begin()+i);
+      permute_rec(ans,new_start,new_left);
+    }
+  }
 };
 
 int main()
