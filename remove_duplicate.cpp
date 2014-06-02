7e217e17ce8a4df4f24f19b42933615de35cde16
xyt <xuyangtong@gmail.com>
2014-06-02 17:05:51 +0800

    finish remove duplicate

--- remove_duplicate.cpp
+++ remove_duplicate.cpp
@@ -4,7 +4,26 @@ using namespace std;
 class Solution {
  public:
   int removeDuplicates(int A[], int n) {
-        
+    if (n <= 1){
+      return n;
+    }
+    int front = 0;
+    int end = 1;
+    while(end < n){
+      if (A[front] == A[end]){
+        end++;
+      }
+      else if (end - 1 > front){
+        front++;
+        A[front] = A[end];
+        end++;
+      }
+      else {
+        front++;
+        end++;
+      }
+    }
+    return front+1;
   }
 };
 
