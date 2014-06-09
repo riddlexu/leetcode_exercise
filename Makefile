f33ac187d8222d1c38ba9c6bdd75acb93d2bd9fa
xyt <xuyangtong@gmail.com>
2014-06-09 12:44:37 +0800

    finish combinations

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-Default: level_traversal_two
+Default: combinations
 CC=g++
 CFLAGS=-O3 -Wall -std=c++11
-PROGRAM=level_traversal_two
+PROGRAM=combinations
 
-level_traversal_two: level_traversal_two.cpp
+combinations: combinations.cpp
 	$(CC) $(CFLAGS) $< -o $@
 
 .PHONY: clean
---
+++ combinations.cpp
@@ -0,0 +1,19 @@
+//https://oj.leetcode.com/problems/combinations/
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int> > combine(int n, int k) {
+        
+  }
+};
+
+int main()
+{
+  int n,k;
+  cin >> n >> k;
+  Solution solution;
+  vector<vector<int> > ans = solution.combine(n,k);
+}
