6200c5ac928a7506b012b317947bd0c68b025a29
xyt <xuyangtong@gmail.com>
2014-06-30 19:49:40 +0800

    combination sum

--- combination_sum.cpp
+++ combination_sum.cpp
@@ -3,9 +3,42 @@
 class Solution {
  public:
   vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
-    return vector<vector<int> > ans;
-    for (int i = 0; i < N; ++i){
-      
+    sort(candidates.begin(), candidates.end());
+    return sort_combinationSum(candidates,target);
+  }
+  vector<vector<int> > sort_combinationSum(vector<int> &candidates, int target) {
+    vector<vector<int> > ans;
+    int now_sum = 0;
+    if (candidates.size() == 0){
+      return ans;
     }
+    
+    int now_element = candidates.back();
+    candidates.pop_back();
+    int element_size = 0;
+    while(target > 0){
+      vector<vector<int> > res = sort_combinationSum(candidates,target);
+      
+      for (int i = 0; i < res.size(); ++i){
+        ans.push_back(res[i]);
+        for (int j = 0; j < element_size; ++j)
+        {
+          ans[ans.size()-1].push_back(now_element);
+        }
+      }
+      target = target - now_element;
+      element_size++;
+      if (target == 0){
+        vector<int> temp;
+        ans.push_back(temp);
+        for (int j = 0; j < element_size; ++j)
+        {
+          ans[ans.size()-1].push_back(now_element);
+        }        
+      }
+    }
+
+    candidates.push_back(now_element);
+    return ans;
   }
 };
