a57cfa30c7223427f19692dfdbe2631a5af7b63c
xuyangtong <xuyangtong@gmail.com>
2014-07-04 00:42:53 +0800

    finish two sum

--- two_sum.cpp
+++ two_sum.cpp
@@ -3,6 +3,24 @@
 class Solution {
  public:
   vector<int> twoSum(vector<int> &numbers, int target) {
-    
+    unordered_set<int> hashset;
+    vector<int> ans;
+    int rindex = -1;
+    for (int i = 0; i < numbers.size(); ++i){
+      if (hashset.find(target-numbers[i]) == hashset.end()){
+        hashset.insert(numbers[i]); 
+      }
+      else{
+        rindex = i;
+        break;
+      }
+    }
+    for (int i = 0; i < rindex; ++i){
+      if (target - numbers[i] == numbers[rindex]){
+        ans.push_back(i+1);
+        ans.push_back(rindex+1);
+        return ans;
+      }
+    }
   }
 };
