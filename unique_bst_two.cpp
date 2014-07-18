031929ad0a943c228de8fdfeddfa961e896a9530
xyt <xuyangtong@gmail.com>
2014-07-18 19:59:42 +0800

    finish unique bst two

--- unique_bst_two.cpp
+++ unique_bst_two.cpp
@@ -1,49 +1,72 @@
 //https://oj.leetcode.com/problems/unique-binary-search-trees-ii/
 
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include<vector>
+#include<stdlib.h>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+
 class Solution {
  public:
   vector<TreeNode *> generateTrees(int n) {
+    if (n <= 0){
+      vector<TreeNode *> ans;
+      ans.push_back(NULL);
+      return ans;
+    }
+    return generateBST(1,n);
+  }
+
+  vector<TreeNode *> generateBST(int start, int end){
     vector<TreeNode *> ans;
-    for (int i = 1; i <= n; ++i){
-      TreeNode *root = new TreeNode(i);
-      for (int j = 1; j <= n; ++j){
-        if (i != j){
-          //insert(root,
-          // i don't know how to do
-          // need to think about it
+    TreeNode* mid;
+    if (start == end){
+      mid = new TreeNode(start);
+      ans.push_back(mid);
+      return ans;
+    }
+      
+    for (int i = start; i <= end; ++i){
+      vector<TreeNode *> left,right;
+      if (i != start){
+        left = generateBST(start,i-1);
+      }
+      if (i != end){
+        right = generateBST(i+1,end);
+      }
+      if (i == start){
+        for (int j = 0; j < right.size(); j++){
+          mid = new TreeNode(i);
+          mid->right = right[j];
+          ans.push_back(mid);
         }
       }
+      else if (i == end){
+        for (int j = 0; j < left.size(); j++){
+          mid = new TreeNode(i);
+          mid->left = left[j];
+          ans.push_back(mid);
+        }
+      }
+      else {
+        for(int j = 0; j < left.size(); j++){
+          for (int k = 0; k < right.size(); k++){
+            mid = new TreeNode(i);
+            mid->left = left[j];
+            mid->right = right[k];
+            ans.push_back(mid);
+          }
+        }
+      }
+      
     }
     return ans;
   }
-
-  void insert(TreeNode *root, int n){
-    if (root->val < n){
-      if (root->left){
-        insert(root->left, n);
-      }
-      else {
-        TreeNode *element = new TreeNode(n);
-        root->left = element;
-      }
-    }
-    else {
-      if (root->right){
-        insert(root->right, n);
-      }
-      else {
-        TreeNode *element = new TreeNode(n);
-        root->right = element;
-      }
-    }
-  }
 };
+
