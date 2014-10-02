441d119ede54bf4f6611a56efb8d78cc18bb5b0a
xuyangtong <xuyangtong@gmail.com>
2014-10-02 22:05:28 +0800

    finish word ladder

--- word_ladder.cpp
+++ word_ladder.cpp
@@ -2,8 +2,8 @@ class Solution {
  public:
   int ladderLength(string start, string end, unordered_set<string> &dict) {
     int tail = start.size() - 1;
-    if(start[tail] == end[0]){
-      return 2;
+    if(start == end){
+      return 1;
     }
     deque<string> parent;
     deque<string> son;
@@ -11,11 +11,26 @@ class Solution {
     int distance = 1;
     while(!dict.empty()){
       while(!parent.empty()){
-        string now = parent.front();
-        if(now[tail] == end[0]){
-          return distance + 1;
+        string now = parent.back();
+        for(int i = 0; i < now.size(); i++){
+          for(char j = 'a'; j <= 'z'; j++){
+            if(now[i] != j){
+              string tmp = now;
+              tmp[i] = j;
+              if(dict.count(tmp) > 0){
+                dict.erase(tmp);
+                son.push_back(tmp);
+                if(tmp == end){
+                  return distance+1;
+                }
+              }
+            }
+          }
         }
-        parent.pop_front();
+        parent.pop_back();
+      }
+      if(son.empty()){
+        return 0;
       }
       swap(parent,son);
       distance++;
