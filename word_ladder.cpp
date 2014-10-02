ab20f95ba696a6910c7b7b160338796ecbba2a22
xuyangtong <xuyangtong@gmail.com>
2014-10-02 13:41:17 +0800

    unfinish word ladder

--- word_ladder.cpp
+++ word_ladder.cpp
@@ -1,6 +1,25 @@
 class Solution {
  public:
   int ladderLength(string start, string end, unordered_set<string> &dict) {
-        
+    int tail = start.size() - 1;
+    if(start[tail] == end[0]){
+      return 2;
+    }
+    deque<string> parent;
+    deque<string> son;
+    parent.push_back(start);
+    int distance = 1;
+    while(!dict.empty()){
+      while(!parent.empty()){
+        string now = parent.front();
+        if(now[tail] == end[0]){
+          return distance + 1;
+        }
+        parent.pop_front();
+      }
+      swap(parent,son);
+      distance++;
+    }
+    return 0;
   }
 };
