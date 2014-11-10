ec6f65710170021277d2d8f6a1ad4a59985ff009
riddlexu <xuyangtong@gmail.com>
2014-11-10 18:34:14 +0800

    finish search rotated sorted array ii

--- search_rotated_sorted_array_ii.cpp
+++ search_rotated_sorted_array_ii.cpp
@@ -5,29 +5,63 @@ class Solution {
   bool search(int A[], int n, int target) {
     int left = 0;
     int right = n-1;
-    while(left < right){
+    while(left <= right){
       int mid = (left + right) / 2;
       if(A[mid] == target){
         return true;
       }
       else if(A[mid] > target){
-        if(A[left] < target){
-          right = mid--;
+        if(A[left] < A[right]){
+          right = mid - 1;
         }
-        else if(A[left] == target){
-          return true;
+        else if(A[left] == A[right]){
+          if(A[left] == target){
+            return true;
+          }
+          else{
+            left++;
+            right--;
+          }
         }
         else {
-          return true;
+          if(A[right] >= A[mid]){ // caution!!!
+            right = mid - 1;
+          }
+          else if(A[left] > target){
+            left = mid + 1;
+          }
+          else{
+            right = mid - 1;
+          }
         }
       }
       else{
-        if(A[
+        if(A[left] < A[right]){
+          left = mid + 1;
+        }
+        else if(A[left] == A[right]){
+          if(A[left] == target){
+            return true;
+          }
+          else {
+            left++;
+            right--;
+          }
+        }
+        else{
+          if(A[left] <= A[mid]){
+            left = mid + 1;
+          }
+          else if(A[left] > target){
+            left = mid + 1;
+          }
+          else{
+            right = mid - 1; //caution!!!
+          }
+        }
       }
     }
-    if(target == A[left]){
-      return true;
-    }
+    
     return false;
   }
 };
