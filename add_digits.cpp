f9589bbd83a1b78299ff03269b308116e28c1e6e
Yangtong Xu <yangtongxu@creditease.cn>
2015-10-09 23:13:56 +0800

    finish add digits

---
+++ add_digits.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/add-digits/
+
+class Solution {
+public:
+  int addDigits(int num) {
+    if (num == 0){
+      return 0;
+    }
+    int ans = num % 9;
+    if (ans == 0){
+      return 9;
+    }
+    return ans;
+  }
+};
