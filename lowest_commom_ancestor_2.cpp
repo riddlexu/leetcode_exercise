b53a52f8cf0b142a303f766cc9b3e07d06a9ac64
Yangtong Xu <yangtongxu@creditease.cn>
2015-11-12 00:03:14 +0800

    finish lca 2

---
+++ lowest_commom_ancestor_2.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * 
+ };
+*/
+class Solution {
+public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (root == NULL){
+      return NULL;
+    }
+    if ((root->val == p->val) || (root->val == q->val)){
+      return root;
+    }
+    TreeNode* x = lowestCommonAncestor(root->left, p, q);
+    TreeNode* y = lowestCommonAncestor(root->right, p, q);
+    if (x && y) {
+      return root;
+    }
+    if (x) {
+      return x;
+    }
+    return y;
+  }
+};
