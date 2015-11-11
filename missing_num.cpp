6516d9ef56775d4ecfad1ffb41d3410a193644d5
Yangtong Xu <yangtongxu@creditease.cn>
2015-11-12 00:08:50 +0800

    finish missing num

---
+++ missing_num.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/missing-number/
+
+class Solution {
+public:
+  int missingNumber(vector<int>& nums) {
+    int sum = 0;
+    for(int i = 0; i < nums.size(); i++) {
+      sum += nums[i];
+              
+    }
+    int res = (1 + nums.size()) * nums.size() / 2;
+    res = res - sum;
+    return res;
+  }
+};
